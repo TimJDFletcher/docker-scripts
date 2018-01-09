aec81acdec04f8483cd9d84af640c2ec6964e10e
Tim Fletcher <tim.fletcher@laterooms.com>
2018-01-09 18:13:36 +0000

    Switch to using a files to write to

--- docker-compose/jinja2-template.py
+++ docker-compose/jinja2-template.py
@@ -7,6 +7,12 @@ from jinja2 import Environment, FileSystemLoader
 image_dir = 'images'
 image_filename = 'image_full_tag.txt'
 docker_tags = {}
+filenames = {}
+
+def parse_cmdline():
+    filenames["inputfile"] = sys.argv[1]
+    filenames["outputfile"] = sys.argv[1].rsplit(".", 1 )[0]
+    return filenames
 
 def load_tags():
     for dirname in os.listdir(image_dir):
@@ -14,13 +20,15 @@ def load_tags():
             with open(image_dir + "/" + dirname + "/" + image_filename, 'r') as file:
                 docker_tags[dirname] = file.read().strip()
                 file.close()
+    return docker_tags
 
-
-def print_template():
+def write_template(filenames, docker_tags):
+    print ("Using", image_dir + " to template",filenames["inputfile"] + " to",filenames["outputfile"])
     # Create the jinja2 environment.
     template = Environment(loader=FileSystemLoader('.'))
-    print (template.get_template(sys.argv[1]).render(env=os.environ,images=docker_tags))
+    with open(filenames["outputfile"], 'w') as file:
+        print (template.get_template(filenames["inputfile"]).render(env=os.environ,images=docker_tags), file=file)
+        file.close()
 
 if __name__ == '__main__':
-    load_tags()
-    print_template()
+    write_template(parse_cmdline(),load_tags())
